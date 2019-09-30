520c1939b6ae1fa14887c99cce8f3c6d3ca5e410
Andre Reinoso Aranda <37244658+Andre-Reinoso@users.noreply.github.com>
2019-09-30 00:00:53 -0500

    Agregando Bootstrap
    Agregando Bootstrap

---
+++ Trabajo Final U1 WEB2/Trabajo Final U1 WEB2/Controllers/Ejercicio3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trabajo_Final_U1_WEB2.Controllers
+{
+    public class Ejercicio3Controller : Controller
+    {
+        // GET: Ejercicio3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
